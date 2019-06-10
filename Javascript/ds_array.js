5b5aa0f048e9fddc670a385a4ce43e5a1b876cae
towfiq046 <towfiqahmed046@gmail.com>
2019-06-10 14:03:28 +0600

    array

---
+++ Javascript/ds_array.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+var array = ["Banana", "Apples", "Oranges", "Blueberries"]
+console.log(array.shift())
+console.log(array.sort())
+console.log(array.push("Kiwi"))
+console.log(array.shift())
+console.log(array.reverse())
+
+var array2 = ["Banana", 
+                ["Apples", 
+                    ["Oranges"], 
+                    "Blueberries"]]
+console.log(array2[1][1][0])
+
+
--- Javascript/keyless_car.js
+++ Javascript/keyless_car.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 function check_driver_age() {
     var age = prompt("What is your age?");
 
